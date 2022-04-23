59abb808dd480cbc91fbc854ced0eba2e7f4df9e
Gabriel <gabrielpacirnediniz@gmail.com>
2022-04-22 21:57:12 -0300

    Adding the modal to register new contacts

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,4 +1,14 @@
-import 'core-js/stable'
-import 'regenerator-runtime/runtime'
+const modalWrapper = document.getElementById("modal-wrapper");
+const newContact = document.getElementById("contact");
+const cancel = document.getElementById("cancel");
 
-import './css/global.css'
+function open() {
+    modalWrapper.classList.add("active");
+}
+
+function close() {
+    modalWrapper.classList.remove("active");
+}
+
+newContact.addEventListener("click", open);
+cancel.addEventListener("click", close);
